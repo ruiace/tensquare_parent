f03a555e59e5c45311fa1d43c2356cc640bb08d2
ruipeng <ruiace@126.com>
2018-12-29 07:43:27 +0800

    代码构建

---
+++ tensquare_base/src/main/java/com/tensquare/base/BaseApplication.java
@@ -0,0 +1,27 @@
+package com.tensquare.base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import util.IdWorker;
+
+/**
+ * 〈一句话功能简述〉<br>
+ * 〈基础项目启动类〉
+ *
+ * @author rp
+ * @create 2018/12/19
+ */
+@SpringBootApplication
+public class BaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaseApplication.class,args);
+    }
+
+    @Bean
+    public IdWorker idWorker(){
+        return new IdWorker(1,1);
+    }
+
+}
---
+++ tensquare_base/src/main/java/com/tensquare/base/pojo/Label.java
@@ -0,0 +1,96 @@
+package com.tensquare.base.pojo;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * 〈一句话功能简述〉<br>
+ * 〈标签实体类〉
+ *
+ * @author rp
+ * @create 2018/12/19
+ */
+@Entity
+@Table(name = "tb_label")
+public class Label {
+
+    /**
+     * id 主键
+     */
+    @Id
+    private String id;
+
+    /**
+     * 标签名称
+     */
+    private String lablename;
+
+    /**
+     * 状态 文本 0：无效 1：有效
+     */
+    private String state;
+
+    /**
+     *  使用数量
+     */
+    private Long count;
+
+    /**
+     * 关注数
+     */
+    private Long fans;
+
+    /**
+     * 是否推荐 0：不推荐 1:推荐
+     */
+    private String recommend;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getLablename() {
+        return lablename;
+    }
+
+    public void setLablename(String lablename) {
+        this.lablename = lablename;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public Long getCount() {
+        return count;
+    }
+
+    public void setCount(Long count) {
+        this.count = count;
+    }
+
+    public Long getFans() {
+        return fans;
+    }
+
+    public void setFans(Long fans) {
+        this.fans = fans;
+    }
+
+    public String getRecommend() {
+        return recommend;
+    }
+
+    public void setRecommend(String recommend) {
+        this.recommend = recommend;
+    }
+}
